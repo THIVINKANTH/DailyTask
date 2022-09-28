b1fbfbb6aef61e2131a0a075debf0ffdb084b895
THIVINKANTH <thivinkanth@gmail.com>
2022-09-28 17:32:33 +0530

    task

---
+++ src/main/java/DailyTask/Task/Swap.java
@@ -0,0 +1,16 @@
+package DailyTask.Task;
+
+public class Swap {
+
+	public static void main(String[] args) {
+	   int x=10;
+	   int y=20;
+	   int swap=x;
+	   x=y;
+	   y=swap;
+	   System.out.println(x);
+	   System.out.println(y);
+
+	}
+
+}
